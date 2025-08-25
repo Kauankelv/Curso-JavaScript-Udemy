f2523d455f59ab88671cd9eaecb59b0b0ca3e720
´´Kauan_kelvin´´ <kauanbernaldo88@gmail.com>
2025-08-25 01:54:02 -0300

    Tipos de dados primitivos

---
+++ JS/Aula 9/index.js
@@ -0,0 +1,18 @@
+const nome = `Luiz`; //string
+const nome1 = "Luiz"; //string
+const nome2 = 'Luiz'; //string
+const num1 = 10; //number
+const num2 = 10.52 //number
+let nomeAluno; //undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memória
+const aprovado = false; // Boolean = true, false (lógico)
+
+const a = [1, 2];
+const b = a;
+
+console.log(a, b);
+
+b.push(3);
+console.log(a, b);
+
+console.log(typeof aprovado, aprovado);
