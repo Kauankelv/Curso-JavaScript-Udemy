8fe4f0c0334878a8ffdf1a77627238f10e79b8e5
´´Kauan_kelvin´´ <kauanbernaldo88@gmail.com>
2025-08-26 20:13:06 -0300

    Atividade feita e resolvida

--- JS/Aula 11/js/script.js
+++ JS/Aula 11/js/script.js
@@ -1,1 +1,12 @@
+//Alert é uma função do comando Window
 alert('alerta');
+
+confirm('olá!');
+
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert(resultado);
