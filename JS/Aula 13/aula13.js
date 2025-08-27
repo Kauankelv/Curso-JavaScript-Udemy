1dcdb13617e8a7caffe21185f5f62947ae7a41bc
´´Kauan_kelvin´´ <kauanbernaldo88@gmail.com>
2025-08-26 21:32:49 -0300

    Mais sobre strings

---
+++ JS/Aula 13/aula13.js
@@ -0,0 +1,21 @@
+//               01234567
+let umaString = 'O rato roeu a roupa do rei de roma.';
+
+/*
+console.log(umaString.concat(' ', 'em', ' ', 'um', ' ', 'lindo dia.'));
+console.log(umaString + ' em um lindo dia.');
+console.log(`${umaString} em um lindo dia`);
+console.log(umaString.lastIndexOf('U', 3));
+console.log(umaString.match(/[a-z]/));
+console.log(umaString.search(/x/)); procura caracter
+console.log(umaString.replace('Um', 'Outra')); 
+console.log(umaString.replace(/r/g, '#')); muda o r por #
+console.log(umaString.length); fala a quantidade de caracteres
+console.log(umaString.slice(2, 6));
+console.log(umaString.slice(-5, -1)); corta
+console.log(umaString.split('r')); separa
+console.log(umaString.toUpperCase()); maiuscula
+console.log(umaString.toLowerCaseCase()); minuscula
+*/
+
+console.log(umaString.toLowerCaseCase());
