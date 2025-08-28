ab45a0158e8a32401494d3f68012d3e7992d9c7e
´´Kauan_kelvin´´ <kauanbernaldo88@gmail.com>
2025-08-28 14:50:42 -0300

    Objeto Math

---
+++ JS/Aula 15/index.js
@@ -0,0 +1,12 @@
+let num1 = 9;
+//console.log(num1 ** ou 0.5(1/2)); metodo pra achar a raiz quadrada
+
+//let num2 = Math.floor(num1); arredonda pra cima
+//let num2 = Math.ceil(num1); arredonda pra baixo
+//let num2 = Math.round(num1); arredonda pro mais proximo
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500)); pega o maior numero da sequencia
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500)); pega o menor numero da sequencias
+//const aleatorio = Math.round(Math.random() * (10-5) + 5);
+//console.log(Math.pow(2, 10)); eleva o primeiro ao segundon
+
+//console.log(Math.pow(2, 10));
